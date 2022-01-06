cd8c5beb58ed0a372c57fda5fadef131d020e913
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2022-01-06 15:53:22 -0700

    d

--- mission2/wwwroot/js/intro413.js
+++ mission2/wwwroot/js/intro413.js
@@ -3,6 +3,7 @@
 $("#calcbtn").click(function () {
 
     // calculates grade using input values
+    // decimals represent values of each assignment type from syllabus
     grade = ($("#txtass").val() * .55) + ($("#txtquiz").val() * .10) + ($("#txtexams").val() * .20) + ($("#txtgp").val() * .05) + ($("#txtINTEX").val() * .10)
 
     // signals an alert when button is clicked to show user their final grade
