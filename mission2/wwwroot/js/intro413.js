f14d3e48eca099fe0382e8ee3c33b8141f51033d
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2022-01-06 15:28:05 -0700

    Initial check-in of module mission2

---
+++ mission2/wwwroot/js/intro413.js
@@ -0,0 +1,14 @@
+﻿
+
+$("#calcbtn").click(function () {
+
+    // calculates grade using input values
+    grade = ($("#txtass").val() * .55) + ($("#txtquiz").val() * .10) + ($("#txtexams").val() * .20) + ($("#txtgp").val() * .05) + ($("#txtINTEX").val() * .10)
+
+    // signals an alert when button is clicked to show user their final grade
+    alert("Grade: " + grade.toFixed(2));
+
+})
+
+
+
